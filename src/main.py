3d3104882ba434a7b352524fa7229f72854d9145
satel31 <blokhnina.tatiana@yandex.ru>
2023-05-02 20:44:55 +0300

    Added docstrings

--- src/main.py
+++ src/main.py
@@ -6,13 +6,17 @@ from src.functions import get_repos_stats
 from src.postgres_db import PostgresDB
 
 
-def main():
-    # собрать статистику по репозиториям пользователя
+def main() -> None:
+    """Основной интерфейс программы"""
+
+    # Запрашиваем имя пользователя для поиска
     print('Введите имя пользователя на GitHub для получения информации по репозиториям')
     username: str = input()
+
+    # Собираем статистику по репозиториям пользователя
     repos: list[dict] = get_repos_stats(username)
 
-    # сохранения данных в базу данных
+    # Загружаем данные для подключения к БД из локального файла env
     load_dotenv()
 
     db_config = {
@@ -22,37 +26,51 @@ def main():
         'port': os.getenv('port'),
         'dbname': os.getenv('dbname')
     }
+
+    # Запрашиваем имя таблицы
     print('Введите имя таблицы')
     table_name: str = input()
+
+    # Создаем экземпляр PostgresDB, при инициализации которого создаётся таблица
     db = PostgresDB(table_name=table_name, **db_config)
+    # Сохраняем данные в БД
     db.insert_data_to_db(repos)
     print(f'Данные успешно записаны в таблицу {table_name}')
 
-    # экспортировать данные в формате JSON
+    # Экспортируем данные в файл JSON
     db.insert_data_to_json()
     print(f'Данные успешно записаны в файл {table_name}.json')
-    sorting: str = input('Хотите отсортировать данные? Да/нет ')
 
+    # Запрашиваем про необходимость сортировки и ограничение количества результатов
+    sorting: str = input('Хотите отсортировать данные? Да/нет ')
     limiting: str = input('Хотите ограничить количество результатов? Да/нет ')
 
+    # Выводим данные в зависимости от ответов: отсортированные и ограниченные, отсортированные, ограниченные, все
     if sorting.lower() == 'да' and limiting.lower() == 'да':
         sort_by: str = input('Введите имя колонки для сортировки: title, owner, forks, language, repository_url')
         limit_to: int = int(input('Введите количество результатов'))
         data: list[dict] = db.read_db(sort_by, limit_to)
+
         for d in data:
             print(d)
+
     elif sorting.lower() == 'да':
         sort_by: str = input('Введите имя колонки для сортировки: title, owner, forks, language, repository_url')
         data: list[dict] = db.read_db(sort_by)
+
         for d in data:
             print(d)
+
     elif limiting.lower() == 'да':
         limit_to = int(input('Введите количество результатов'))
         data: list[dict] = db.read_db(limit_to)
+
         for d in data:
             print(d)
+
     else:
         data: list[dict] = db.read_db()
+
         for d in data:
             print(d)
 
--- src/postgres_db.py
+++ src/postgres_db.py
@@ -5,16 +5,22 @@ import psycopg2
 class PostgresDB:
     """Обеспечивает взаимодействие с базой данных Postgres"""
 
-    def __init__(self, table_name: str, dbname: str, user: str, password: str, host: str = 'localhost', port: str = '5432'):
-        self.conn = psycopg2.connect(host=host, database=dbname, user=user, password=password, port=port)
-        self.cur = self.conn.cursor()
-        self.conn.autocommit = True
-        self.table_name: str = table_name
+    def __init__(self, table_name: str, dbname: str, user: str, password: str, host: str = 'localhost', port: str = '5432') -> None:
+        """При инициализации объекта создаётся соединение, курсор и таблица"""
 
+        # Создаем соединение
+        self.conn = psycopg2.connect(host=host, database=dbname, user=user, password=password, port=port)
+        # Создаем курсор
+        self.cur = self.conn.cursor()
+        # Создаем автокоммит
+        self.conn.autocommit = True
+
+        # Создаем таблицу
+        self.table_name: str = table_name
         self.create_table()
 
-    def create_table(self):
-        """Создать таблицу"""
+    def create_table(self) -> None:
+        """Создаём таблицу в БД"""
         with self.conn:
             self.cur.execute(f"""
                 CREATE TABLE {self.table_name} (
@@ -27,8 +33,8 @@ class PostgresDB:
                 )
             """)
 
-    def insert_data_to_db(self, repositories: list[dict]):
-        """Добавить данные в таблицу"""
+    def insert_data_to_db(self, repositories: list[dict]) -> None:
+        """Добавляем данные в таблицу в БД"""
         with self.conn:
             for repository in repositories:
                 self.cur.execute(
@@ -38,28 +44,36 @@ class PostgresDB:
                     """,
                     (repository['name'], repository['owner']['login'], repository['forks'],
                      repository['language'], repository['html_url'])
-                )
+                    )
 
-    def insert_data_to_json(self):
-        """Экспортировать данные в формат JSON"""
+    def insert_data_to_json(self) -> None:
+        """Считываем данные из БД и экспортируем в файл JSON"""
+        # Задаём название файла по названию таблицы
         file = f'{self.table_name}.json'
+        # Считываем данные из БД
         repos_dict: list[dict] = self.read_db()
+        # Записываем данные в файл
         with open(file, 'a', encoding='utf-8') as f:
             json.dump(repos_dict, f, ensure_ascii=False)
 
-    def read_db(self, sort_by: str = None, limit: int = None):
-        """Получить данные из таблицы."""
+    def read_db(self, sort_by: str = None, limit: int = None) -> list[dict]:
+        """Получаем и возвращаем данные из таблицы с сортировкой и/или ограничением или без"""
         with self.conn:
+            # Данные с сортировкой и ограничением
             if sort_by and limit:
                 self.cur.execute(f"""SELECT * FROM {self.table_name} ORDER BY {sort_by} LIMIT {limit}""")
+            # Данные только с сортировкой
             elif sort_by:
                 self.cur.execute(f"""SELECT * FROM {self.table_name} ORDER BY {sort_by}""")
+            # Данные только с ограничением
             elif limit:
                 self.cur.execute(f"""SELECT * FROM {self.table_name} LIMIT {limit}""")
+            # Все данные без сортировки
             else:
                 self.cur.execute(f"""SELECT * FROM {self.table_name}""")
             repos_data: list[tuple] = self.cur.fetchall()
             repos_dict = []
+            # Преобразуем данные в список словарей репозиториев
             for data in repos_data:
                 repos_dict.append({'repository_id': data[0],
                                    'title': data[1],
